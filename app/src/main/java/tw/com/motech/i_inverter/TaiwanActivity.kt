880f25c2732b4d3f8984be160090029fe9231422
amau712 <amau712@gmail.com>
2021-01-19 10:24:52 +0800

    台灣頁面

---
+++ app/src/main/java/tw/com/motech/i_inverter/TaiwanActivity.kt
@@ -0,0 +1,14 @@
+package tw.com.motech.i_inverter
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class TaiwanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_taiwan)
+    }
+}
