a3cf19dde01bba41ff68b49d1508a5b5ffc30e7f
amau712 <amau712@gmail.com>
2021-03-23 11:35:18 +0800

    螢幕旋轉後都預設為0 = fragmentA

--- app/src/main/java/tw/com/motech/i_inverter/SiteFuncActivity.kt
+++ app/src/main/java/tw/com/motech/i_inverter/SiteFuncActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_site_func.*
 
 class SiteFuncActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_site_func)
+
+        // 螢幕旋轉後都預設為0 = fragmentA
+        savedInstanceState?.putInt("menu_index", 0)
 
         var bottom_nav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         val fragmentA = FragmentA()
@@ -30,6 +34,14 @@ class SiteFuncActivity : AppCompatActivity() {
             }
             true
         }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        // Read the state of item position
+        var menu_index = savedInstanceState.getInt("menu_index")
+        bottom_nav.menu.getItem(menu_index).isChecked =  true
     }
 
     private fun setCurrentFragment(fragment: Fragment)=
